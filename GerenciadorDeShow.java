b3f8b13242e51d8ad97b5cb66bad3398f46a6e1d
Guilherme Rodrigues Tavares <guilherme.rtavares@outlook.com>
2024-06-24 20:47:33 -0300

    Interface desenvolvida

--- GerenciadorDeShow.java
+++ GerenciadorDeShow.java
@@ -3,9 +3,5 @@ package model.entities;
 import java.util.ArrayList;
 
 public interface GerenciadorDeShow {
-    void salvarShow(Show show);
-    ArrayList<Show> getListaShows();
-    void verificarListaVazia() throws Exception;
-    Show buscarShow(String nomeTurne) throws Exception;
-    void apagarShow(Show show);
+    
 }
